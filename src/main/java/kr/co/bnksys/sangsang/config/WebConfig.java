e420d84f007aaa73a7444fd0806d57a881bf3f29
sjkim <voidsjkim@gmail.com>
2025-08-21 22:00:03 +0900

    김승준-CrossOrigins 모두 허용으로 변경

--- src/main/java/kr/co/bnksys/sangsang/config/WebConfig.java
+++ src/main/java/kr/co/bnksys/sangsang/config/WebConfig.java
@@ -13,9 +13,17 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3001")  // ✅ 와일드카드 X, 정확히 지정
+                        /*
+                        .allowedOrigins("http://localhost:3001")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowCredentials(true);
+
+                         */
+                        .allowedOriginPatterns("*")   // 전체 Origin 허용
+                        .allowedMethods("*")          // 모든 HTTP 메소드 허용
+                        .allowedHeaders("*")          // 모든 헤더 허용
+                        .allowCredentials(true);      // 쿠키/인증정보 허용
             }
         };
     }
